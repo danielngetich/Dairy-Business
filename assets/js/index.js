8c7b6e0834352873736ea96c4de4bda4727d4163
Moringa School <daniel.ngetich@student.moringaschool.com>
2022-07-16 13:12:42 +0300

    add more functions to js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,26 @@
-function totalProduction()
-
-let shades = [];
+function production(){
+    // document.addEventListener('DOMContentLoaded', ()=>{
+    //     Document.getElementById('btn');
+    // } );
+    let shades = [];
  
-const production = (ev)=>{
-    ev.preventDefault();
-}
-let shade = {
-    shadeA:document.getElementById("shade-a"),
-    shadeB:document.getElementById("shade-b"),
-    shadeC:document.getElementById("shade-c"),
-    shadeD:document.getElementById("shade-d"),
-}
-shades.push(shade);
-document.forms[0].reset();
+    const production = (ev)=>{
+        ev.preventDefault();
+    }
+    // let shade = {
+    //     shadeA:document.getElementById("shade-a").value,
+    //     shadeB:document.getElementById("shade-b").value,
+    //     shadeC:document.getElementById("shade-c").value,
+    //     shadeD:document.getElementById("shade-d").value,
+    // }
+    // shades.push(shade);
+    document.getElementById("shadeA").value = "Your production in Shed A litres per day\n";
+    // document.getElementById("shadeB").value = "Your production in Shed B" +  + " litres per day";
+    // document.getElementById("shadeC").value = "Your production in Shed C" +  + " litres per day";
+    // document.getElementById("shadeD").value = "Your production in Shed D" +  + " litres per day";
+    // document.getElementById("total").value = "The total production is" +  + "litres per day";
+    // e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', ()=>{
-    Document.getElementById('btn').addEventListener('click',production);
-} );
+}
+
+
