603a134b130706cfe2a6f4c47ef67311e41f1607
Moringa School <daniel.ngetich@student.moringaschool.com>
2022-07-16 17:36:18 +0300

    add js functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,7 +8,7 @@ document.getElementById("btn").onclick = function production(ev){
     }
     let shades = [];
     shades.push(shade);
-    let total = 
+    let sum = [shadea.value + shadeb.value + shadec.value + shaded.value];
     document.getElementById("shadeA").value = "Your production in Shed A is " + shadea.value +" litres per day";
     document.getElementById("shadeB").value = "Your production in Shed B is " + shadeb.value +" litres per day";
     document.getElementById("shadeC").value = "Your production in Shed C is " + shadec.value +" litres per day";
@@ -16,7 +16,7 @@ document.getElementById("btn").onclick = function production(ev){
     document.getElementById("total").value = "The total production is "+ total.value+ " litres per day";
    
     ev.preventDefault();
-    production();
+    total();
 }
 
 
