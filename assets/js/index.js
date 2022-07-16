2139f990e0006fcd38aec739760a1625bb85bd7a
Moringa School <daniel.ngetich@student.moringaschool.com>
2022-07-16 16:19:41 +0300

    add more functions to js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,21 @@
-function production(){
-    // document.addEventListener('DOMContentLoaded', ()=>{
-    //     Document.getElementById('btn');
-    // } );
-    let shades = [];
- 
-    const production = (ev)=>{
-        ev.preventDefault();
-    }
-    // let shade = {
-    //     shadeA:document.getElementById("shade-a").value,
-    //     shadeB:document.getElementById("shade-b").value,
-    //     shadeC:document.getElementById("shade-c").value,
-    //     shadeD:document.getElementById("shade-d").value,
-    // }
-    // shades.push(shade);
-    document.getElementById("shadeA").value = "Your production in Shed A litres per day\n";
-    // document.getElementById("shadeB").value = "Your production in Shed B" +  + " litres per day";
-    // document.getElementById("shadeC").value = "Your production in Shed C" +  + " litres per day";
-    // document.getElementById("shadeD").value = "Your production in Shed D" +  + " litres per day";
-    // document.getElementById("total").value = "The total production is" +  + "litres per day";
-    // e.preventDefault();
 
+document.getElementById("btn").onclick = function production(ev){
+    let shade = {
+        shadeA:document.getElementById("shadea").value,
+        shadeB:document.getElementById("shadeb").value,
+        shadeC:document.getElementById("shadec").value,
+        shadeD:document.getElementById("shaded").value,
+    }
+    let shades = [];
+    shades.push(shade);
+
+    document.getElementById("shadeA").value = "Your production in Shed A is"+ shadeA +  "litres per day";
+    document.getElementById("shadeB").value = "Your production in Shed B litres per day";
+    document.getElementById("shadeC").value = "Your production in Shed C litres per day";
+    document.getElementById("shadeD").value = "Your production in Shed D litres per day";
+    document.getElementById("total").value = "The total production is litres per day";
+   
+    ev.preventDefault();
 }
 
 
