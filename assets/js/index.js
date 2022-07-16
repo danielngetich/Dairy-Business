81b47de944b9a7e548478aa78521963b43290b9c
Moringa School <daniel.ngetich@student.moringaschool.com>
2022-07-16 18:05:25 +0300

    add styles to css

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,15 +8,14 @@ document.getElementById("btn").onclick = function production(ev){
     }
     let shades = [];
     shades.push(shade);
-    let sum = [shadea.value + shadeb.value + shadec.value + shaded.value];
+    const total = parseInt(shadea.value + shadeb.value + shadec.value + shaded.value);
     document.getElementById("shadeA").value = "Your production in Shed A is " + shadea.value +" litres per day";
     document.getElementById("shadeB").value = "Your production in Shed B is " + shadeb.value +" litres per day";
     document.getElementById("shadeC").value = "Your production in Shed C is " + shadec.value +" litres per day";
     document.getElementById("shadeD").value = "Your production in Shed D is " + shaded.value +" litres per day";
-    document.getElementById("total").value = "The total production is "+ total.value+ " litres per day";
+    document.getElementById("total").value = "The total production is "+ total+ " litres per day";
    
     ev.preventDefault();
-    total();
 }
 
 
