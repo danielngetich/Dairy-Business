6218bb041d3eb6c99c095780d7b65f8be7ad1605
Moringa School <daniel.ngetich@student.moringaschool.com>
2022-07-16 17:07:28 +0300

    add functions to js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,22 @@
 
 document.getElementById("btn").onclick = function production(ev){
     let shade = {
-        shadeA:document.getElementById("shadea").value,
-        shadeB:document.getElementById("shadeb").value,
-        shadeC:document.getElementById("shadec").value,
-        shadeD:document.getElementById("shaded").value,
+        shadeA:document.getElementById("shadea"),
+        shadeB:document.getElementById("shadeb"),
+        shadeC:document.getElementById("shadec"),
+        shadeD:document.getElementById("shaded"),
     }
     let shades = [];
     shades.push(shade);
-
-    document.getElementById("shadeA").value = "Your production in Shed A is"+ shadeA +  "litres per day";
-    document.getElementById("shadeB").value = "Your production in Shed B litres per day";
-    document.getElementById("shadeC").value = "Your production in Shed C litres per day";
-    document.getElementById("shadeD").value = "Your production in Shed D litres per day";
+    let total = 
+    document.getElementById("shadeA").value = "Your production in Shed A is " + shadea.value +" litres per day";
+    document.getElementById("shadeB").value = "Your production in Shed B is " + shadeb.value +" litres per day";
+    document.getElementById("shadeC").value = "Your production in Shed C is " + shadec.value +" litres per day";
+    document.getElementById("shadeD").value = "Your production in Shed D is " + shaded.value +" litres per day";
     document.getElementById("total").value = "The total production is litres per day";
    
     ev.preventDefault();
+    production();
 }
 
 
