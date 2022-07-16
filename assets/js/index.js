069b3e4136c70cd347a6b181728509bf3f85d121
Moringa School <daniel.ngetich@student.moringaschool.com>
2022-07-16 11:50:17 +0300

    add event listener

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,17 @@
 let shades = [];
-let totalProduction = {
+ 
+const production = (ev)=>{
+    ev.preventDefault();
+}
+let shade = {
     shadeA:document.getElementById("shadea"),
     shadeB:document.getElementById("shadeb"),
     shadeC:document.getElementById("shadec"),
     shadeD:document.getElementById("shaded"),
 }
-shades.push(totalProduction);
+shades.push(shade);
 document.forms[0].reset();
+
 document.addEventListener('DOMContentLoaded', ()=>{
-    Document.getElementById('btn').addEventListener('click',totalProduction);
+    Document.getElementById('btn').addEventListener('click',production);
 } );
